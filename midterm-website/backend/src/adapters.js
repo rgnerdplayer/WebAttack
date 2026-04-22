2a001436899104afe10037f0babe0ac6079d4111
曾憲揚 <cengxianyang@cengxianyangdeMacBook-Pro-3.local>
2026-04-22 12:26:30 +0800

    fix: downgrade prisma to v5 and sync database

--- midterm-website/backend/src/adapters.js
+++ midterm-website/backend/src/adapters.js
@@ -1,2 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient(); // 這就是你的資料庫操作員
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient({
+  // 告訴伺服器執行時，請連這條有 pooler 的網址
+  datasourceUrl: process.env.DATABASE_URL,
+});
